2c2305196ac5d4187e768ed0713963589a4649b2
Edward Sun <edward.sun2015@gmail.com>
2025-05-01 20:44:43 -0700

    readme

--- collect_demos.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# press ESC to finish recording
-python collect_demos.py --robots Panda --camera agentview --environment PickPlaceSA --directory ./demos
-
-# some problem with using the --camera flag
-

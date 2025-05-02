8d35b28dc9153a1f789ef8e6031c2427b0d3e199
Edward Sun <edward.sun2015@gmail.com>
2025-05-01 19:58:22 -0700

    update with macros fix

--- test.py
+++
@@ -1,7 +0,0 @@
-from sa_suite.controllers import SpaceMouse, SpaceMouseConfig
-
-space_mouse_config = SpaceMouseConfig()
-spacemouse = SpaceMouse(space_mouse_config)
-
-while True:
-    print(spacemouse.get_controller_state())
